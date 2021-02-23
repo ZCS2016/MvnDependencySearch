4a918776cace976ab7d1a3c8b1828e0e3f7f11f8
cszhu <cszhu@chinaums.com>
2021-02-23 15:14:55 +0800

    MvnDependencySearch

---
+++ src/main/java/com/example/MvnDependencySearch/MvnDependencySearchApplication.java
@@ -0,0 +1,13 @@
+package com.example.MvnDependencySearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvnDependencySearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvnDependencySearchApplication.class, args);
+	}
+
+}
